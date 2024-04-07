f964018b80fc4c8389298e474182ee046e85cd39
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2024-04-07 23:23:54 +0500

    final_1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const placesList = document.querySelector('.places__list');
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+function createCard(cardName, cardLink) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+    const cardDelete = cardElement.querySelector(".card__delete-button");
+    
+    cardElement.querySelector(".card__image").src = cardLink;
+    cardElement.querySelector('.card__title').textContent = cardName;
+
+    
+    cardDelete.addEventListener('click', function() {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData.name, cardData.link);
+    placesList.append(cardElement);
+});
+
